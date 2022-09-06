8d9a66885e465761707ea810296e005a96c1cdc0
GastonLell <gastonglell@gmail.com>
2022-09-06 09:13:25 -0300

    ejercicio uno

---
+++ src/com/ejercicio1/EjercicioUno.java
@@ -0,0 +1,20 @@
+package com.ejercicio1;
+
+public class EjercicioUno {
+    public static void main(String[] args){
+        int number = 5;
+        System.out.println(number);
+
+        long numberLong = 123123;
+        System.out.println(numberLong);
+
+        double numberDouble = 123.123;
+        System.out.println(numberDouble);
+
+        boolean bool = true;
+        System.out.println(bool);
+
+        String cadena = "Gaston Lell";
+        System.out.println(cadena);
+    }
+}
